58b97af8bd7e771e5906369e0bf447d7ce8a0783
guilhermemartines <126878401+guilhermemartines@users.noreply.github.com>
2023-11-06 08:38:27 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-alert('Olá mundo');
+// Pom ----------------------------------------
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//enquanto
+while(contador < listaDeTeclas.length ) {
+    
+    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
+
+
+
